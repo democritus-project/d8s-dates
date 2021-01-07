206d0da2c923e55c2f4b6c86eeb31ed5128969d2
Floyd Hightower <floyd.hightower27@gmail.com>
2021-01-07 11:02:53 -0500

    Initial commit of code

---
+++ dem_dates/__init__.py
@@ -0,0 +1,7 @@
+# this is quoted with triple quotes so that names with apostrophes in them will work properly
+
+__author__ = '''Floyd Hightower'''
+__email__ = 'floyd.hightower27@gmail.com'
+__version__ = '2021.01.07'
+
+from .dem_dates import *
---
+++ dem_dates/temp_utils.py
@@ -0,0 +1,33 @@
+"""Utility functions temporarily provided until the rest of the democritus functions get uploaded."""
+
+import functools
+
+
+def number_zero_pad(num: int, length: int) -> str:
+    """."""
+    if length < len(str(num)):
+        message = 'The length you provided is shorter than the number. Please provide a length that is at least as long as the given number.'
+        raise ValueError(message)
+
+    zero_padded_number = f'{num}'
+
+    while len(zero_padded_number) < length:
+        zero_padded_number = f'0{zero_padded_number}'
+
+    return zero_padded_number
+
+
+def date_parse_first_argument(func):
+    """."""
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        from dem_dates import date_parse
+
+        date_arg = args[0]
+        other_args = args[1:]
+
+        parsed_date_arg = date_parse(date_arg)
+        return func(parsed_date_arg, *other_args, **kwargs)
+
+    return wrapper
