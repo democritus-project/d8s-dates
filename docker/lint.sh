352ee7e6ec03e5f12abc480b37dce0295140f8b3
Floyd Hightower <floyd.hightower27@gmail.com>
2021-02-17 06:56:31 -0500

    Improving linting/testing services

--- docker/lint.sh
+++ docker/lint.sh
@@ -6,12 +6,15 @@ echo "Running linters and formatters..."
 
 black democritus_dates/ tests/
 
+isort democritus_dates/ tests/
+
 pylint --fail-under 9 democritus_dates/*.py
 
 mypy democritus_dates/ tests/
 
-isort democritus_dates/ tests/
-
 flake8 democritus_dates/ tests/
 
 bandit democritus_dates/
+
+# we run black again at the end to undo any odd changes made by any of the linters above (e.g. isort)
+black democritus_dates/ tests/
